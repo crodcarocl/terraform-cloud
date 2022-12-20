9bf7aad0c5610c74b0b91cfeb46a4377a811817d
Carlos Rodriguez <crodcaro@icloud.com>
2022-12-20 16:21:01 -0300

    [@crodcarocl] Big changes

--- security_group.tf
+++ security_group.tf
@@ -9,8 +9,8 @@ resource "aws_security_group_rule" "web_http_in" {
     type = "ingress"
     from_port = 80
     to_port = 80
-    protocol = tcp
-    cidr_block = ["0.0.0.0/0"]
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
 
     security_group_id = aws_security_group.web.id
 }
@@ -19,8 +19,8 @@ resource "aws_security_group_rule" "web_https_in" {
     type = "ingress"
     from_port = 443
     to_port = 443
-    protocol = tcp
-    cidr_block = ["0.0.0.0/0"]
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
 
     security_group_id = aws_security_group.web.id
 }
@@ -30,7 +30,7 @@ resource "aws_security_group_rule" "web_egress_in" {
     from_port = 0
     to_port = 0
     protocol = "-1"
-    cidr_block = ["0.0.0.0/0"]
+    cidr_blocks = ["0.0.0.0/0"]
 
     security_group_id = aws_security_group.web.id
 }
