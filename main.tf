e18e2cfa917304e4571f31dc0c00631036a08225
Carlos Rodriguez <crodcaro@icloud.com>
2022-12-20 17:43:09 -0300

    [@crodcarocl] Big changes

--- main.tf
+++ main.tf
@@ -3,9 +3,9 @@ resource "aws_instance" "web" {
   instance_type = var.instance_type
 
   subnet_id = module.vpc.private_subnets[0]
-  vpc_security_group_ids = [aws_security_group.web.id]
+  vpc_security_group_ids = [module.web_sg.security_group_id]
 
   tags = {
-    Name = "HelloWorld"
+    Name = "ec2-web"
   }
 }
