6e9fe2f7c8c66bbfb6c38e82d15f9eabe89b772b
Carlos Rodriguez <crodcaro@icloud.com>
2022-12-21 12:18:35 -0300

    Test

--- vpc.tf
+++ vpc.tf
@@ -1,13 +1,13 @@
 module "vpc" {
   source = "./modules/vpc"
 
-  name = var.vpc_configs.vpc_name
-  cidr = var.vpc_configs.vpc_cidr
+  name = var.vpc_config.vpc_name
+  cidr = var.vpc_config.vpc_cidr
 
   azs             = var.vpc_config.vpc_azs
   private_subnets = var.vpc_config.vpc_private_subnets
   public_subnets  = var.vpc_config.vpc_public_subnets
 
-  nat_gateway_tags = var.project_configs.project_tags
-  tags = var.project_configs.project_tags
+  nat_gateway_tags = var.project_config.project_tags
+  tags = var.project_config.project_tags
 }
