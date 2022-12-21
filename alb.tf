b74d2cbc31f80a630ca1a524137ca49fbc8c7c31
Carlos Rodriguez <crodcaro@icloud.com>
2022-12-21 13:52:17 -0300

    Test

--- alb.tf
+++ alb.tf
@@ -1,8 +1,8 @@
 module "web_alb" {
   source  = "./modules/alb"
 
-  name               = var.alb_config.alb_name "web-loadblancer"
-  load_balancer_type = var.alb_config.load_balancer_type "application"
+  name               = var.alb_config.alb_name
+  load_balancer_type = var.alb_config.load_balancer_type
 
   tags = var.project_config.tags
   env_name = var.environment.env_name
