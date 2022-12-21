3357f5163ee8419c17c60c62868d691c86900a0e
Carlos Rodriguez <crodcaro@icloud.com>
2022-12-21 14:05:18 -0300

    Test

--- alb.tf
+++ alb.tf
@@ -4,6 +4,10 @@ module "web_alb" {
   name               = var.alb_config.alb_name
   load_balancer_type = var.alb_config.load_balancer_type
 
+  vpc_id             = module.vpc.vpc_id
+  subnets            = module.vpc.public_subnets
+  security_groups    = [module.web_sg.security_group_id]
+
   tags = var.project_config.project_tags
   env_name = var.environment.env_name
 }
--- modules/alb/variables.tf
+++ modules/alb/variables.tf
@@ -1,3 +1,19 @@
+variable "vpc_id" {
+    description = "Loadbalancer VPC"
+    type = string
+}
+
+variable "subnets" {
+    description = "Loadbalancer subnets"
+    type = list(string)
+}
+
+variable "security_groups" {
+    description = "Loadbalancer security groups"
+    type = list(string)
+}
+
+
 variable "name" {
     description = "Loadbalancer Name"
     type = string
