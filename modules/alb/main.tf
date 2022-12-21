497c528f5d0bf6b2b5c85d076e2f711f6a4369fc
Carlos Rodriguez <crodcaro@icloud.com>
2022-12-21 14:06:08 -0300

    Test

--- modules/alb/main.tf
+++ modules/alb/main.tf
@@ -5,9 +5,9 @@ module "alb" {
   name               = "${var.env_name}-${var.name}"
   load_balancer_type = var.load_balancer_type
 
-  vpc_id             = module.vpc.vpc_id
-  subnets            = module.vpc.public_subnets
-  security_groups    = [module.web_sg.security_group_id]
+  vpc_id             = var.vpc_id
+  subnets            = var.subnets
+  security_groups    = var.security_groupsss
 
   target_groups = [
     {
