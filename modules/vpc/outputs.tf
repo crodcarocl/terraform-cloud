df81a62b0c37847fddecc3921d0c2eea1333aba0
Carlos Rodriguez <crodcaro@icloud.com>
2022-12-21 13:31:53 -0300

    Test

---
+++ modules/vpc/outputs.tf
@@ -0,0 +1,14 @@
+output "vpc_id" {
+    description = "VPC Id"
+    value = module.vpc.vpc_id
+}
+
+output "private_subnets" {
+    description = "VPC Id"
+    value = module.vpc.private_subnets
+}
+
+output "public_subnets" {
+    description = "VPC Id"
+    value = module.vpc.public_subnets
+}
