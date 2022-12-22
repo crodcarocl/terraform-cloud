cf66468fefe602d76038c343f96a12b2cd16fa17
Carlos Rodriguez <crodcaro@icloud.com>
2022-12-22 12:47:46 -0300

    [@crodcarocl] Fix variables and outputs

--- modules/sg/main.tf
+++ modules/sg/main.tf
@@ -1,9 +1,9 @@
-module "web_sg" {
+module "securitygroup" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "4.16.2"
 
   name   = var.name
-  vpc_id = var.vpc.vpc_id
+  vpc_id = var.vpc_id
 
   ingress_rules = var.ingress_rules
   ingress_cidr_blocks = var.ingress_cidr_blocks
--- securitygroup.tf
+++ securitygroup.tf
@@ -1,12 +1,12 @@
 module "web_sg" {
   source = "./modules/sg"
 
-  name   = var.asg_config.sg_name
+  name   = var.sg_config.sg_name
   vpc_id = module.vpc.vpc_id
 
-  ingress_rules       = var.asg_config.ingress_rules
-  ingress_cidr_blocks = var.asg_config.ingress_cidr_blocks
+  ingress_rules       = var.sg_config.ingress_rules
+  ingress_cidr_blocks = var.sg_config.ingress_cidr_blocks
 
-  egress_rules       = var.asg_config.egress_rules
-  egress_cidr_blocks = var.asg_config.egress_cidr_blocks
+  egress_rules       = var.sg_config.egress_rules
+  egress_cidr_blocks = var.sg_config.egress_cidr_blocks
 }
