4e05f5ccda46598595b16e12435cfe5edae5fbec
Carlos Rodriguez <crodcaro@icloud.com>
2022-12-22 12:43:10 -0300

    [@crodcarocl] Fix typo

--- securitygroup.tf
+++ securitygroup.tf
@@ -4,9 +4,9 @@ module "web_sg" {
   name   = var.asg_config.sg_name
   vpc_id = module.vpc.vpc_id
 
-  ingress_rules       = var.asg_cong.ingress_rules
-  ingress_cidr_blocks = var.asg_cong.ingress_cidr_blocks
+  ingress_rules       = var.asg_config.ingress_rules
+  ingress_cidr_blocks = var.asg_config.ingress_cidr_blocks
 
-  egress_rules       = var.asg_cong.egress_rules
-  egress_cidr_blocks = var.asg_cong.egress_cidr_blocks
+  egress_rules       = var.asg_config.egress_rules
+  egress_cidr_blocks = var.asg_config.egress_cidr_blocks
 }
