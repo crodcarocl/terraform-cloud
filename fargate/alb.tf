0122437d48eae6c60886f59d100d2b72c3e4b2f3
Carlos Rodriguez <crodcaro@icloud.com>
2022-12-26 16:01:25 -0300

    [@crodcarocl] Fix alb variables

--- fargate/alb.tf
+++ fargate/alb.tf
@@ -6,7 +6,7 @@ module "ecs_alb" {
 
   vpc_id             = module.vpc.vpc_id
   subnets            = module.vpc.public_subnets
-  security_groups    = [module.web_sg.security_group_id]
+  security_groups    = [module.ecs_sg.security_group_id]
 
   tags      = var.project_config.project_tags
   env_short = var.environment.env_short
