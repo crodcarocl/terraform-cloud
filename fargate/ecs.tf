3647e69a74c13a6497d4415b08c609924daaccbc
Carlos Rodriguez <crodcaro@icloud.com>
2022-12-26 16:41:03 -0300

    [@crodcarocl] First fargate try

--- fargate/ecs.tf
+++ fargate/ecs.tf
@@ -13,18 +13,29 @@ module "ecs" {
     }
   }
 
-  fargate_capacity_providers = {
-    FARGATE = {
-      default_capacity_provider_strategy = {
-        weight = 50
-      }
-    }
-    FARGATE_SPOT = {
-      default_capacity_provider_strategy = {
-        weight = 50
-      }
-    }
-  }
-
   tags = var.project_config.project_tags
+}
+
+module "ecs-fargate-task-definition" {
+  source  = "cn-terraform/ecs-fargate-task-definition/aws"
+  version = "1.0.30"
+  # insert the 1 required variable here
+
+  name_prefix = "task"
+}
+
+module "ecs-fargate-service" {
+  source  = "cn-terraform/ecs-fargate-service/aws"
+  version = "2.0.37"
+  # insert the 7 required variables here
+
+  container_name      = "tomcat-service"
+  ecs_cluster_arn     = module.ecs.cluster_arn
+  name_prefix         = "test"
+
+  vpc_id              = module.vpc.vpc_id
+  private_subnets     = module.vpc.private_subnets
+  public_subnets      = module.vpc.public_subnets
+
+  task_definition_arn = module.ecs-fargate-task-definition.aws_ecs_task_definition_td_arn
 }
