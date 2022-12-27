491a4370a1da5306c2e85843084d45e83a662919
Carlos Rodriguez <crodcaro@icloud.com>
2022-12-27 11:38:14 -0300

    [@crodcarocl] Unleash the fargate

--- fargate/output.tf
+++ fargate/output.tf
@@ -6,18 +6,18 @@ output "lb_arn" {
   value = module.ecs_alb.lb_arn
 }
 
-# output "security_group_id" {
-#   value = module.ecs_sg.security_group_id
-# }
+output "security_group_id" {
+  value = module.ecs_sg.security_group_id
+}
 
-# output "ecs_cluster_arn" {
-#   value = module.ecs.cluster_arn
-# }
+output "ecs_cluster_arn" {
+  value = module.ecs.cluster_arn
+}
 
-# output "ecs_cluster_name" {
-#   value = module.ecs.cluster_name
-# }
+output "ecs_cluster_name" {
+  value = module.ecs.cluster_name
+}
 
-# output "aws_ecs_task_definition_td_arn" {
-#   value = module.ecs-fargate-task-definition.aws_ecs_task_definition_td_arn
-# }
+output "aws_ecs_task_definition_td_arn" {
+  value = module.ecs-fargate-task-definition.aws_ecs_task_definition_td_arn
+}
