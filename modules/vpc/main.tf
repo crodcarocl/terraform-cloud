ee51b04a621ce0b390b4b77ca5d6801a958c9de7
Carlos Rodriguez <crodcaro@icloud.com>
2023-01-19 20:19:58 -0300

    Testing github actions 9

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -1,8 +1,3 @@
-resource "aws_eip" "nat" {
-  count = 1
-  vpc = true
-}
-
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
   version = "3.18.1"
@@ -17,9 +12,6 @@ module "vpc" {
   enable_nat_gateway = true
   single_nat_gateway = true
   one_nat_gateway_per_az = false
-
-  reuse_nat_ips       = true                    # <= Skip creation of EIPs for the NAT Gateways
-  external_nat_ip_ids = "${aws_eip.nat.*.id}"   # <= IPs specified here as input to the module
 
   nat_gateway_tags = var.tags
   tags = var.tags
